12c207a85d22d1b6a2f45973fa87a6b3a88f7a62
Healy <diarmuid.healy@accenture.com>
2021-08-18 16:34:32 +0100

    Java project added

---
+++ src/main/java/prjXrayDemo/Calculator.java
@@ -0,0 +1,31 @@
+package prjXrayDemo;
+/**
+ *
+ * @author diarmuid.healy
+ */
+public class Calculator 
+{
+    public double addNum(double num1, double num2)
+    {
+        double res = num1 + num2;
+        return res;
+    }
+    
+    public double subNum(double num1, double num2)
+    {
+        double res = num1 - num2;
+        return res;
+    }
+    
+    public double multNum(double num1, double num2)
+    {
+        double res = num1 * num2;
+        return res;
+    }
+    
+    public double divNum(double num1, double num2)
+    {
+        double res = num1 / num2;
+        return res;
+    }
+}
